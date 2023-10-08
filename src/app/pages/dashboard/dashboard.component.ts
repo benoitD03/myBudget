06280e708624907ab3a5979d0f85b716315577a5
benoitD03 <bens.92@hotmail.fr>
2023-10-08 08:31:44 +0200

    Début construction page Dashboard

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {Categorie} from "../../class/categorie";
+import {CategorieService} from "../../services/categorie.service";
+import {AccountService} from "../../services/account.service";
 
 @Component({
   selector: 'app-dashboard',
@@ -6,5 +9,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent {
+  categories: Categorie[] = []
 
+  constructor(private categorieService: CategorieService, private accountService: AccountService) {
+  }
+  ngOnInit() {
+    const id_User = + !this.accountService.getIdUser();
+    this.categorieService.getCategoriesByUserId(id_User).subscribe(
+      (data) => {
+        this.categories=data;
+        console.log(this.categories)
+      },
+      (error) => {
+        console.error('Erreur lors de la récupération des catégories :', error);
+      }
+    )
+  }
 }
