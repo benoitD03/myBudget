24a0afafdbadd22f0b7159dbb2391ce40b4bea2f
benoitD03 <bens.92@hotmail.fr>
2023-11-26 11:27:37 +0100

    Stockage du Thème dans le localStorage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component, ViewChild} from '@angular/core';
+import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
 import {Router} from "@angular/router";
 import {AccountService} from "./services/account.service";
 import {MatSidenav} from "@angular/material/sidenav";
@@ -8,14 +8,22 @@ import {BreakpointObserver} from "@angular/cdk/layout";
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, AfterViewInit{
 
   constructor(private observer: BreakpointObserver,private router: Router, private accountService: AccountService) {
   }
+
   isLightTheme = true
   @ViewChild(MatSidenav)
   sidenav!: MatSidenav;
 
+  ngOnInit() {
+    const storedTheme = localStorage.getItem('theme');
+
+    document.body.setAttribute('data-theme', storedTheme || 'light');
+
+    this.isLightTheme = storedTheme === 'light';
+  }
   ngAfterViewInit() {
     this.observer
       .observe(['(max-width: 1100px)'])
@@ -41,7 +49,9 @@ export class AppComponent {
    * Methode pour changer de thème
    */
   onThemeSwitchChange() {
-    this.isLightTheme = !this.isLightTheme
+    this.isLightTheme = !this.isLightTheme;
+
+    localStorage.setItem('theme', this.isLightTheme ? 'light' : 'dark');
 
     document.body.setAttribute(
       'data-theme',
