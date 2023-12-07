fc241e67ccb775cc729931d00185ef3b21eec721
benoitD03 <bens.92@hotmail.fr>
2023-12-07 13:35:58 +0100

    Ajout Paginator page full year

--- src/app/pages/full-year/full-year.component.ts
+++ src/app/pages/full-year/full-year.component.ts
@@ -1,8 +1,11 @@
-import {AfterViewInit, Component, OnInit} from '@angular/core';
+import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
 import {SousCategorie} from "../../class/sous-categorie";
 import * as moment from "moment";
 import {SousCategorieService} from "../../services/sous-categorie.service";
 import { Chart, ChartConfiguration, ChartData } from 'chart.js';
+import {MatPaginator} from "@angular/material/paginator";
+import {MatTableDataSource} from "@angular/material/table";
+import {MatSort} from "@angular/material/sort";
 
 
 @Component({
@@ -15,13 +18,20 @@ export class FullYearComponent implements OnInit, AfterViewInit{
   sousCategoriesRevenus: SousCategorie[] = [];
   depensesCategories: { categoryName: string; total: number }[] = [];
   Columns: string[] = ['Categorie', 'Nom', 'Image', 'Date', 'Somme'];
+  dataSource!: MatTableDataSource<SousCategorie>;
+  @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator | null = null;
+  @ViewChild(MatSort) sort: MatSort | null = null;
 
 
   constructor(private sousCategorieService: SousCategorieService) {
+
   }
 
   ngOnInit() {
     this.loadList()
+    this.dataSource = new MatTableDataSource<SousCategorie>(this.sousCategoriesDepenses);
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
   }
 
   loadList() {
@@ -32,7 +42,6 @@ export class FullYearComponent implements OnInit, AfterViewInit{
         this.sousCategoriesDepenses=data.filter((sousCategorie : SousCategorie )=> sousCategorie.Depense);
         this.sousCategoriesRevenus=data.filter((sousCategorie : SousCategorie )=> !sousCategorie.Depense);
         this.depensesCategories = this.depensesTotalesParCategories(this.sousCategoriesDepenses);
-        console.log(this.depensesCategories);
       },
       (error) => {
         console.error('Erreur lors de la récupération des transactions :', error);
@@ -66,6 +75,9 @@ export class FullYearComponent implements OnInit, AfterViewInit{
   }
 
   ngAfterViewInit() {
+    this.dataSource = new MatTableDataSource<SousCategorie>(this.sousCategoriesDepenses);
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
     setTimeout(() => {
       this.initializeChart();
     }, 100);
