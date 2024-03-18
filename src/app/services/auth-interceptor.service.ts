a2a93322c917c41be198df1ce532fb4b1ad7cd0e
benoitD03 <bens.92@hotmail.fr>
2024-03-18 18:50:52 +0100

    Add token in interceptor

--- src/app/services/auth-interceptor.service.ts
+++ src/app/services/auth-interceptor.service.ts
@@ -3,15 +3,20 @@ import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HttpErrorResponse
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 import { Router } from '@angular/router';
+import {AccountService} from "./account.service";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthInterceptorService implements HttpInterceptor {
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private serviceAccount: AccountService) { }
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    const token: string | null = this.serviceAccount.getToken();
+    if (token) {
+      req = req.clone({headers: req.headers.set('Authorization', 'Bearer ' + token)});
+    }
     return next.handle(req).pipe(
       catchError((error: HttpErrorResponse) => {
         if (error.status === 401) { // 401 Unauthorized
